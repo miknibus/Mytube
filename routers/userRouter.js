65a8292c7536681029e21eefec36c4bfa7b13eef
Subin Kim <subin.kim@biola.edu>
2020-03-14 09:53:30 -0700

    day 1

---
+++ routers/userRouter.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from '../routes';
+import { users, userDetail, editProfile, changePassword } from '../controller/userController';
+
+const userRouter = express.Router();
+
+userRouter.get(routes.root, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
