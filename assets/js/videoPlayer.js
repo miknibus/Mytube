4fac725ae547d1269ab937317175de9a6aa8a08f
Subin Kim <subin.kim@biola.edu>
2020-04-01 20:17:23 -0700

    day 7

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,5 +1,5 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
-const videoPlayer = videoContainer.querySelector("#jsVideoPlayer video");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
 const fullScrnBtn = document.getElementById("jsFullScreen");
